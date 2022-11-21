b9afea0bd42accb1acbb33e28e02eac86f6f1138
ssharifzoda <sharifzoda.sino@gmail.com>
2022-11-21 07:32:20 +0300

    final

--- internal/botSystem/botLogicCommands.go
+++ internal/botSystem/botLogicCommands.go
@@ -6,14 +6,12 @@ import (
 	"feedback/pkg/logging"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/spf13/viper"
-	"log"
 	"strconv"
 	"strings"
 )
 
 func BotLogicCommands(bot *tgbotapi.BotAPI, update tgbotapi.Update, s *service.Service) {
 	logger := logging.GetLogger()
-	log.Println(update.Message.Chat.ID)
 	text := update.Message.Text
 	repl := strings.ReplaceAll(text, " ", "")
 	sliceStr := strings.Split(repl, "-")
@@ -38,7 +36,6 @@ func Sender(response types.FeedBackTG) {
 
 func ValidateResponseStatus(bot *tgbotapi.BotAPI) {
 	logger := logging.GetLogger()
-	logger.Println("error Atoi func")
 	msg := tgbotapi.NewMessage(int64(viper.GetInt("chatid")), "Неправильно введен ID отзыва, проверьте и отправьте ещё раз")
 	if _, err := bot.Send(msg); err != nil {
 		logger.Println(err)
